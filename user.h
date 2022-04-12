5334b47d409d91e1803a558e3910520ff6a590b3
noahcheeseman <noah.cheeseman@kcl.ac.uk>
2022-04-12 17:46:16 +0100

    move all un-pushable file changes from Krell repo

--- user.h
+++ user.h
@@ -23,7 +23,8 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
-
+int mprotect(void *addr, int len);
+int munprotect(void *addr, int len);
 // ulib.c
 int stat(const char*, struct stat*);
 char* strcpy(char*, const char*);
